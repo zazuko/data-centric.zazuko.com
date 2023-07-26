06be823113c7c7e79e81a974fa7cd01694175415
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2023-07-26 11:02:51 +0200

    temp: base path for gh-pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://data-centric.zazuko.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/data-centric.zazuko.com/',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
