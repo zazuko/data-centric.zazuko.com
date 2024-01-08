5cf27fd077ea4f1cbf05722ac4073ee95ddcda15
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2024-01-08 10:40:36 +0100

    prefer select lines by from and num

--- src/lines.js
+++ src/lines.js
@@ -1,11 +1,11 @@
-export function selectLines(str, { from = 1, to, num } = {}) {
+export function selectLines(str, { from = 1, to = from, num } = {}) {
   const fromChar = str.split('\n', from - 1).join('\n').length
   let toChar
 
-  if (to) {
-    toChar = str.split('\n', to).join('\n').length
-  } else if(num) {
+  if (num) {
     toChar = str.split('\n', from + num).join('\n').length
+  } else {
+    toChar = str.split('\n', to).join('\n').length
   }
 
   return str.substring(fromChar, toChar).trim()
