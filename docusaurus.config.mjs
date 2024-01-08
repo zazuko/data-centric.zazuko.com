e44a09c413c45ac40a58272181166cb9b52a983a
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2024-01-08 10:10:46 +0100

    build: update docosaurus to v3

--- docusaurus.config.mjs
+++ docusaurus.config.mjs
@@ -1,8 +1,7 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+import { themes } from 'prism-react-renderer'
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -129,8 +128,8 @@ const config = {
         copyright: `Copyright © ${new Date().getFullYear()} Zazuko GmbH. Built with Docusaurus.`,
       },
       prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
+        theme: themes.jettwaveLight,
+        darkTheme: themes.jettwaveDark,
         additionalLanguages: ['turtle'],
       },
     }),
