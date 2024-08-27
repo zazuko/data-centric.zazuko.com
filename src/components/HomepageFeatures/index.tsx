428eea8f4b7b88098576e2886721ad6409c7b469
Ludovic Muller <ludovicmuller1@gmail.com>
2024-08-27 15:14:58 +0200

    chore: format some files

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './styles.module.css';
+import React from "react";
+import clsx from "clsx";
+import styles from "./styles.module.css";
 
 type FeatureItem = {
   page: string;
@@ -11,63 +11,58 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    page: '/docs/rdfjs/',
-    title: 'RDF/JS',
-    image: '/img/rdfjs.png',
+    page: "/docs/rdfjs/",
+    title: "RDF/JS",
+    image: "/img/rdfjs.png",
     description: (
       <>
-        Be most productive by using modern, modular JavaScript libraries for creating and manipulating RDF graphs
+        Be most productive by using modern, modular JavaScript libraries for
+        creating and manipulating RDF graphs
       </>
     ),
   },
   {
-    page: '/docs/workflows/',
-    title: 'Workflows',
-    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Barnard_59%2C_part_of_a_vast_dark_cloud.jpg/257px-Barnard_59%2C_part_of_a_vast_dark_cloud.jpg',
+    page: "/docs/workflows/",
+    title: "Workflows",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Barnard_59%2C_part_of_a_vast_dark_cloud.jpg/257px-Barnard_59%2C_part_of_a_vast_dark_cloud.jpg",
     description: (
-      <>
-        Automate ETL (extract, transform, load) tasks focused on Linked Data.
-      </>
+      <>Automate ETL (extract, transform, load) tasks focused on Linked Data.</>
     ),
   },
   {
-    page: '/docs/cubes/',
-    title: 'Cubes',
-    image: '/img/cube.png',
+    page: "/docs/cubes/",
+    title: "Cubes",
+    image: "/img/cube.png",
     description: (
-      <>
-        Use cube.link Cube Schema to create and manage RDF data cubes
-      </>
+      <>Use cube.link Cube Schema to create and manage RDF data cubes</>
     ),
   },
   {
-    page: '/docs/apps/',
-    title: 'Applications',
-    image: '/img/gears.webp',
-    description: (
-      <>
-        Data-centric based applications
-      </>
-    )
+    page: "/docs/apps/",
+    title: "Applications",
+    image: "/img/gears.webp",
+    description: <>Data-centric based applications</>,
   },
   {
-    page: '',
-    title: 'More',
-    image: 'https://cygri.github.io/rdf-logos/png/rdf-128.png',
+    page: "",
+    title: "More",
+    image: "https://cygri.github.io/rdf-logos/png/rdf-128.png",
     description: (
       <>
-        Use a variety of Semantic Web technologies such as SHACL or n3 to build data-centric applications
+        Use a variety of Semantic Web technologies such as SHACL or n3 to build
+        data-centric applications
       </>
     ),
   },
 ];
 
-function Feature({page, title, image, description}: FeatureItem) {
+function Feature({ page, title, image, description }: FeatureItem) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">
         <a href={page}>
-          <img alt="feature image" src={image} style={{height: "128px"}} />
+          <img alt="feature image" src={image} style={{ height: "128px" }} />
         </a>
       </div>
       <div className="text--center padding-horiz--md">
@@ -88,8 +83,23 @@ export default function HomepageFeatures(): JSX.Element {
           <div className="col col--2"></div>
           <div className="col col--8">
             <div className="text--center">
-              <p>The digital world has evolved significantly since the advent of graphical user interfaces in the 1980s. Today, we interact with countless applications across multiple systems, creating an abundance of data that holds untapped potential. To realize the full value of this information, a paradigm shift is needed—one that places data at the center, rather than applications.</p>
-              <p>Zazuko is dedicated to becoming the leading provider of <a href="http://datacentricmanifesto.org/">data-centric</a> tools and applications, fostering an open, data-centric ecosystem where both developers and end-users can thrive. We aim to break down barriers between domain specialists and the insights they seek, enabling them to derive valuable information from raw data and use it to solve real-world problems.</p>
+              <p>
+                The digital world has evolved significantly since the advent of
+                graphical user interfaces in the 1980s. Today, we interact with
+                countless applications across multiple systems, creating an
+                abundance of data that holds untapped potential. To realize the
+                full value of this information, a paradigm shift is needed—one
+                that places data at the center, rather than applications.
+              </p>
+              <p>
+                Zazuko is dedicated to becoming the leading provider of{" "}
+                <a href="http://datacentricmanifesto.org/">data-centric</a>{" "}
+                tools and applications, fostering an open, data-centric
+                ecosystem where both developers and end-users can thrive. We aim
+                to break down barriers between domain specialists and the
+                insights they seek, enabling them to derive valuable information
+                from raw data and use it to solve real-world problems.
+              </p>
             </div>
           </div>
         </div>
